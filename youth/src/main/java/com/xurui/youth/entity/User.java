2ea206b4014b6d0a96e16e2c3c4e1a170d767a9f
xurui <xu_rui@outlook.com>
2018-04-13 16:00:13 +0800

    add 集成mybatis，使用mybatis-generator生成entity、mapper

---
+++ youth/src/main/java/com/xurui/youth/entity/User.java
@@ -0,0 +1,62 @@
+package com.xurui.youth.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 用户表
+ * Created by XuRui on 2018/4/13.
+ */
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+    * 主键id
+    **/
+    private Long id;
+
+    /**
+    * 账户
+    **/
+    private String account;
+
+    /**
+    * 名称
+    **/
+    private String name;
+
+    /**
+    * 密码
+    **/
+    private String password;
+
+    /**
+    * 用户类型：1普通，0管理员
+    **/
+    private Byte type;
+
+    /**
+    * 用户状态：1正常，0停用
+    **/
+    private Byte status;
+
+    /**
+    * 是否已被删除： 1是，0否
+    **/
+    private Byte isDeleted;
+
+    /**
+    * 创建时间
+    **/
+    private Date gmtCreated;
+
+    /**
+    * 修改时间
+    **/
+    private Date gmtModified;
+
+}
