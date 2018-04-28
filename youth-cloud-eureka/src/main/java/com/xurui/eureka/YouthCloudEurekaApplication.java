d8c2ae1dce62adfb1a50581625c36b47e825be69
xurui <xu_rui@outlook.com>
2018-04-28 14:28:53 +0800

    add 集成spring cloud eureka，作为服务发现组件

---
+++ youth-cloud-eureka/src/main/java/com/xurui/eureka/YouthCloudEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.xurui.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class YouthCloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthCloudEurekaApplication.class, args);
+	}
+}
