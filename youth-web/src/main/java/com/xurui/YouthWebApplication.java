072f4dcbd79b989fd3274d3b9d9116dfaad02b3d
xurui <xu_rui@outlook.com>
2019-10-10 17:25:41 +0800

    add 启用eureka

--- youth-web/src/main/java/com/xurui/YouthWebApplication.java
+++ youth-web/src/main/java/com/xurui/YouthWebApplication.java
@@ -3,7 +3,9 @@ package com.xurui;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableDubboConfiguration
 @SpringBootApplication
 public class YouthWebApplication {
--- youth/src/main/java/com/xurui/YouthApplication.java
+++ youth/src/main/java/com/xurui/YouthApplication.java
@@ -3,7 +3,16 @@ package com.xurui;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 启动类
+ *
+ * @author XuRui
+ * @date 2018/4/19
+ */
+
+@EnableDiscoveryClient
 @EnableDubboConfiguration
 @SpringBootApplication
 public class YouthApplication {
